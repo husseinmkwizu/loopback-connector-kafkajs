06e35a2080be9ab7c29bd198765a5894d8fb0c17
Hussein Mkwizu <hussein.mkwizu@gmail.com>
2021-09-16 19:47:01 +0300

    use kafkajs logger, update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,47 @@
-import { Kafka, logLevel, ProducerRecord } from 'kafkajs'
+import { Kafka, logLevel, ProducerRecord } from "kafkajs";
 
 exports.initialize = function (dataSource, cb) {
-    var settings = dataSource.settings;
-    const { debug, host } = settings
-    if (debug) {
-        console.log("KAFKAJS -- INITIALIZE CALLED")
+  var settings = dataSource.settings;
+  const { debug, host } = settings;
+
+  //brokers
+  let brokers = null;
+  if (typeof host === "string") {
+    brokers = host.split(",");
+  }
+
+  const kafka = new Kafka({
+    clientId: settings?.clientId ?? "kafkajs-producer",
+    brokers: brokers,
+    sasl: settings?.sasl,
+    ssl: settings?.ssl,
+    retry: settings?.retry,
+    logLevel: debug ? logLevel.DEBUG : logLevel.NOTHING,
+  });
+
+  const producer = kafka.producer();
+
+  if (debug) {
+    this.producer.logger().info("KAFKAJS -- Calling producer.connect");
+  }
+
+  producer.connect().then(() => {
+    this.producer.logger().info("KAFKAJS -- Producer ready ");
+  });
+
+  var connector = new KafkaConnector(producer, { debug });
+  dataSource.connector = connector;
+  connector.DataAccessObject = function () {};
+  for (var m in KafkaConnector.prototype) {
+    var method = KafkaConnector.prototype[m];
+    if ("function" === typeof method) {
+      connector.DataAccessObject[m] = method.bind(connector);
+      for (var k in method) {
+        connector.DataAccessObject[m][k] = method[k];
+      }
     }
-
-    //brokers
-    let brokers = null
-    if (typeof host === 'string') {
-        brokers = host.split(",")
-    }
-
-    const kafka = new Kafka({
-        clientId: settings?.clientId ?? 'kafkajs-producer',
-        brokers: brokers,
-        sasl: settings?.sasl,
-        ssl: settings?.ssl,
-        retry: settings?.retry,
-        logLevel: debug ? logLevel.DEBUG : logLevel.NOTHING
-    });
-
-    const producer = kafka.producer();
-
-    producer.connect()
-        .then(() => { console.log("Producer ready "); })
-
-    var connector = new KafkaConnector(producer, { debug });
-    dataSource.connector = connector;
-    connector.DataAccessObject = function () { };
-    for (var m in KafkaConnector.prototype) {
-        var method = KafkaConnector.prototype[m];
-        if ("function" === typeof method) {
-            connector.DataAccessObject[m] = method.bind(connector);
-            for (var k in method) {
-                connector.DataAccessObject[m][k] = method[k];
-            }
-        }
-    }
-    cb && cb();
+  }
+  cb && cb();
 };
 
 /**
@@ -48,46 +50,48 @@ exports.initialize = function (dataSource, cb) {
  *  @param {Object} The kafka-node producer
  */
 function KafkaConnector(producer, options) {
-    this.name = "kafka";
-    this._models = {};
-    this.producer = producer;
-    this.options = options
+  this.name = "kafka";
+  this._models = {};
+  this.producer = producer;
+  this.options = options;
 }
 
 KafkaConnector.prototype.send = function (payload: ProducerRecord, cb) {
-    const { debug } = this.options
-    if (debug) {
-        console.log("KAFKAJS -- SEND CALLED")
-    }
+  const { debug } = this.options;
+  if (debug) {
+    this.producer.logger().info("KAFKAJS -- SEND CALLED");
+  }
 
-    this.producer.send(payload)
-        .then(response => {
-            cb(null, response)
-        }).catch(error => {
-            cb(error, null)
-        });
+  this.producer
+    .send(payload)
+    .then((response) => {
+      cb(null, response);
+    })
+    .catch((error) => {
+      cb(error, null);
+    });
 };
 
 function setRemoting(fn, options) {
-    options = options || {};
-    for (var opt in options) {
-        if (options.hasOwnProperty(opt)) {
-            fn[opt] = options[opt];
-        }
+  options = options || {};
+  for (var opt in options) {
+    if (options.hasOwnProperty(opt)) {
+      fn[opt] = options[opt];
     }
-    fn.shared = true;
+  }
+  fn.shared = true;
 }
 
 setRemoting(KafkaConnector.prototype.send, {
-    description: "Send a message to Kafka server",
-    accepts: [
-        {
-            arg: "payload",
-            type: "object",
-            description: "Message payload",
-            http: { source: "body" },
-        },
-    ],
-    returns: { arg: "data", type: "object", root: true },
-    http: { verb: "post", path: "/" },
+  description: "Send a message to Kafka server",
+  accepts: [
+    {
+      arg: "payload",
+      type: "object",
+      description: "Message payload",
+      http: { source: "body" },
+    },
+  ],
+  returns: { arg: "data", type: "object", root: true },
+  http: { verb: "post", path: "/" },
 });
